c7f11ea5b3df8999f95d46553ad7e10305d22d19
Sagar Jadhav <sagar@buddyboss.com>
2019-02-23 16:25:14 +0530

    Updates

---
+++ precache-manifest.0ed9fd9d7c42c4ec911176e3b471e4ab.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "badbdc3a441319a6b488",
+    "url": "/react-app/static/js/main.badbdc3a.chunk.js"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "d27bc1fb9004c29f4d65f38f3fdee288",
+    "url": "/react-app/index.html"
+  }
+];
